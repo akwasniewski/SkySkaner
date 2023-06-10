c5e733796c68097c49bf9aa4155c8106c72f7340
bartefix <bartek.procyk@student.uj.edu.pl>
2023-06-10 23:03:20 +0200

    usuwanie

--- src/main/java/com/skyskaner/skyskaner/ReservationController.java
+++ src/main/java/com/skyskaner/skyskaner/ReservationController.java
@@ -48,12 +48,13 @@ void setHowmany(ActionEvent a){
 
 void makeReservation() {
     for (Flight f : result.flights) {
+
         int id = f.id;
-        String query = "DO $$ DECLARE i INTEGER;BEGIN FOR i IN 1.." + howmany + " LOOP EXECUTE 'insert into user_tickets SELECT id_ticket, " + HelloApplication.userId + "," + ischild + "," + luggagetype + " from tickets t join flights f on t.id_flight = f.id_flight where f.id_flight=" + id + " order by t.price desc limit 1';END LOOP;END $$;";
+        String query = "DO $$ DECLARE i INTEGER;BEGIN FOR i IN 1.." +howmany+ " LOOP EXECUTE 'insert into user_tickets SELECT id_ticket, " + HelloApplication.userId + "," + ischild + "," + luggagetype + " from tickets t join flights f on t.id_flight = f.id_flight where f.id_flight=" + id + " order by t.price desc limit 1';END LOOP;END $$;";
         try (Statement stmt = DatabaseHandler.connection.createStatement()) {
             stmt.executeQuery(query);
         } catch (SQLException e) {
-
+          //  throw new RuntimeException(e);
         }
         }
     }
